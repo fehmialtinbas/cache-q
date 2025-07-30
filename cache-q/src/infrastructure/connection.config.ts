43165f0a0b5ebf7aab95fc0338433f5e8a85805a
Fehmi Altinbas <fehmialtinbas@Fehmis-MacBook-Pro.local>
2025-07-30 18:50:51 +0300

    dynamic configuration

--- cache-q/src/infrastructure/connection.config.ts
+++ cache-q/src/infrastructure/connection.config.ts
@@ -1,12 +1,32 @@
-//if you want to get env params from .env file with process you this
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+const service = new ConfigService();
 export const applicationDBConfig = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  host: service.get('DB_HOST'),
+  port: service.get('DB_PORT'),
+  database: service.get('DB_NAME'),
+  username: service.get('DB_USERNAME'),
+  password: service.get('DB_PASSWORD'),
   synchronize: true,
-  logging: true,
+  logging: false,
   entities: ['libs/**/*.model{.ts,.js}'],
 } as any;
+
+//this is the dynamic module configuration for TypeORM
+export const envConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    database: configService.get('DB_NAME'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    synchronize: true,
+    logging: true,
+    entities: ['libs/**/*.model{.ts,.js}'],
+  }),
+  inject: [ConfigService],
+};
