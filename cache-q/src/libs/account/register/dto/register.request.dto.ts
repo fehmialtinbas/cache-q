933cadb4b985aafd5f36be49cd790f3773fe30fc
Fehmi Altinbas <fehmialtinbas@Fehmis-MacBook-Pro.local>
2025-08-14 00:12:22 +0300

    class-validator

--- cache-q/src/libs/account/register/dto/register.request.dto.ts
+++ cache-q/src/libs/account/register/dto/register.request.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateTaskDto {
   @ApiProperty({ example: 'User Name', description: 'Kullanıcı adı' })
+  @IsString()
   userName: string;
   @ApiProperty({ example: 'Email', description: 'Email' })
+  @IsString()
   email: string;
 }
--- cache-q/src/libs/task/dto/task.request.dto.ts
+++ cache-q/src/libs/task/dto/task.request.dto.ts
@@ -1,11 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
 import { TaskStatus } from '../domain/task.types';
 
 export class CreateTaskDto {
   @ApiProperty({ example: 'Task Name', description: 'Görev adı' })
+  @IsString()
   name: string;
   @ApiProperty({ example: 'Task Description', description: 'Görev açıklaması' })
+  @IsString()
   description?: string;
   @ApiProperty({ example: 'pending', description: 'Görev durumu' })
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
 }
